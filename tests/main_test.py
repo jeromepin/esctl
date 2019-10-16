19f629c66f61fcb84b225def41167c59f0ad5815
Jérôme Pin <jeromepin@users.noreply.github.com>
2019-10-16 11:59:33 +0200

    Implement TestFindScheme

---
+++ tests/main_test.py
@@ -0,0 +1,54 @@
+from esctl.main import Esctl
+from esctl.config import Context
+from base_test_class import EsctlTestCase
+
+
+class TestFindScheme(EsctlTestCase):
+    def setUp(self):
+        self.esctl = Esctl()
+        self.esctl.context = Context("foo")
+
+    def test_scheme_discovery(self):
+        cases = [
+            {
+                'input': [
+                    "https://foo.example.com:8200"
+                ],
+                'expected_output': 'https'
+            },
+            {
+                'input': [
+                    "http://foo.example.com:8200"
+                ],
+                'expected_output': 'http'
+            },
+            {
+                'input': [
+                    "https://foo.example.com:8200"
+                    "http://bar.example.com:8200"
+                    "http://baz.example.com:8200"
+                ],
+                'expected_output': 'https'
+            },
+            {
+                'input': [
+                    "foo://foo.example.com:8200"
+                ],
+                'expected_output': 'https'
+            },
+            {
+                'input': [
+                    "foo.example.com:8200"
+                ],
+                'expected_output': 'https'
+            },
+        ]
+
+        for case in cases:
+            self.esctl.context.cluster = {
+                "servers": case.get("input")
+            }
+            self.assertEqual(
+                self.esctl.find_scheme(),
+                case.get('expected_output')
+            )
