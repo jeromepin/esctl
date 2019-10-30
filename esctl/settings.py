fdbd241adb484d5a81d7a950b5c889b55ee81c3f
Jérôme Pin <jeromepin@users.noreply.github.com>
2019-10-30 11:50:20 +0100

    Add some typing since support start as Python 3.5+

--- esctl/settings.py
+++ esctl/settings.py
@@ -1,5 +1,6 @@
 import logging
 from abc import ABC
+from typing import Any
 
 from esctl.main import Esctl
 
@@ -11,7 +12,7 @@ class Settings(ABC):
 
 
 class Setting:
-    def __init__(self, name, value, persistency="defaults"):
+    def __init__(self, name: str, value: Any, persistency: str = "defaults"):
         self.name = name
         self.value = value
         self.persistency = persistency
@@ -20,26 +21,20 @@ class Setting:
 class ClusterSettings(Settings):
     """Handle cluster-level settings."""
 
-    def get(self, key, persistency="transient"):
+    def get(self, key: str, persistency: str = "transient") -> Setting:
         settings = Esctl._es.cluster.get_settings(
             include_defaults=True, flat_settings=True
         )
 
         if key in settings.get(persistency):
-            return Setting(
-                key, settings.get(persistency).get(key), persistency
-            )
+            return Setting(key, settings.get(persistency).get(key), persistency)
         else:
             # If the setting cannot be found in the requested persistency
             # look for it in the "defaults" values
             if key in settings.get("defaults"):
-                return Setting(
-                    key, settings.get("defaults").get(key), "defaults"
-                )
+                return Setting(key, settings.get("defaults").get(key), "defaults")
             else:
-                return Setting(key, None, None)
+                return Setting(key, None)
 
-    def set(self, sections, value, persistency="transient"):
-        return Esctl._es.cluster.put_settings(
-            body={persistency: {sections: value}}
-        )
+    def set(self, sections: str, value, persistency: str = "transient"):
+        return Esctl._es.cluster.put_settings(body={persistency: {sections: value}})
