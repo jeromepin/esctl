a5ff4d40bfa5d211b1ee6ffdb4b1345484b67390
Jérôme Pin <jeromepin@users.noreply.github.com>
2019-11-15 15:33:21 +0100

    Implement node exclusion

--- esctl/cmd/node.py
+++ esctl/cmd/node.py
@@ -1,6 +1,59 @@
 from esctl.commands import EsctlCommand, EsctlLister
 from esctl.main import Esctl
 from esctl.formatter import JSONToCliffFormatter
+from esctl.utils import Color
+
+
+class NodeExclude(EsctlCommand):
+    """Define a list of excluded nodes from routing."""
+
+    def take_action(self, parsed_args):
+        setting_name = "cluster.routing.allocation.exclude.{}".format(parsed_args.by)
+
+        if parsed_args.list is None:
+            setting = self.cluster_settings.get(setting_name, persistency="transient")
+
+            if setting.value is None:
+                setting.value = ""
+
+            self.print_success(setting.value)
+
+        else:
+            self.print_output(
+                ("Changing node exclusion list" " ({}) to : {}").format(
+                    Color.colorize(setting_name, Color.ITALIC),
+                    Color.colorize(parsed_args.list, Color.ITALIC),
+                )
+            )
+            self.print_success(
+                self.cluster_settings.set(
+                    setting_name, parsed_args.list, persistency="transient"
+                )
+            )
+
+    def get_parser(self, prog_name):
+        parser = super().get_parser(prog_name)
+
+        parser.add_argument(
+            "--by",
+            help=(
+                "Attribute to filter by (default: _ip). "
+                "This can be a custom string or one of the built-ins: "
+                " _ip, _name, _host."
+            ),
+            default="_ip",
+        )
+        parser.add_argument(
+            "list",
+            metavar="<list>",
+            nargs="?",
+            help=(
+                "Comma-separated list of values nodes should have to be"
+                " excluded from shard allocation"
+            ),
+        )
+
+        return parser
 
 
 class NodeHotThreads(EsctlCommand):
